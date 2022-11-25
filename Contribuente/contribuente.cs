9e69ad22a64e756dd70c342e1df43595f8b5a806
Aras Pourbijan <arasmidos@gmail.com>
2022-11-25 12:08:59 +0100

    Add project files.

---
+++ Contribuente/contribuente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contribuente
+{
+      
+    internal class Contribuente
+    {
+        public String Nome { get; set; }
+        public String Cognome { get; set; }
+        public DateTime DataNascita { get; set; } 
+        public string CodiceFiscale { get; set; }   
+        public bool IsMale { get; set; }
+        public string ComuneResidenza { get; set; } 
+        public decimal RedditoAnnuale { get; set; } 
+
+        public Contribuente() { }
+        public Contribuente(string Nome, String Cognome, DateTime DataNascita, string CodiceFiscale, bool IsMale, string ComuneResidenza, decimal RedditoAnnuale)
+        { 
+           
+        }
+    }
+}
